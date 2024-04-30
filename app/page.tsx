95214699781a4e75a6234eb17cfb1c508864f330
Matheus Amon <amonmatheus757@gmail.com>
2024-04-30 07:17:00 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return (
-    <div className="px-20 bg-red-500 text-zinc-100">
-      <h1>Hello, Tailwind!</h1>
-    </div>
-  );
-}
- 
+  return <Header />;
+};
+
 export default Home;
