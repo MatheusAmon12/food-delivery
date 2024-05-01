a38e6a3b5f8e533e87c8f80b633fa2d79b14d341
Matheus Amon <amonmatheus757@gmail.com>
2024-05-01 15:06:46 -0300

    fix: show discount percentage correctly

--- app/_components/discount-percentage.tsx
+++ app/_components/discount-percentage.tsx
@@ -10,10 +10,7 @@ const DiscountPercentage = ({ product }: DiscountPercentageProps) => {
     <div className="flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
       <ArrowDownIcon size={12} />
       <span className="text-xs font-semibold">
-        {
-          //adicionar valor do db
-          product.discountPercentage ? product.discountPercentage : "10%"
-        }
+        {product.discountPercentage}%
       </span>
     </div>
   );
