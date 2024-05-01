08d0259373971e16dead6f602d1c86ed96be8814
Matheus Amon <amonmatheus757@gmail.com>
2024-05-01 15:46:20 -0300

    feat: add recommended page

---
+++ app/products/recommended/page.tsx
@@ -0,0 +1,36 @@
+import Header from "@/app/_components/header";
+import ProductItem from "@/app/_components/product-item";
+import { db } from "@/app/_lib/prisma";
+
+const RecomendedProductsPage = async () => {
+  const products = await db.product.findMany({
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="px-6 py-6">
+        <h2 className="mb-6 text-lg font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecomendedProductsPage;
