416e8e05b0ce2ad6c3f08c7ae438b873b639e93c
Matheus Amon <amonmatheus757@gmail.com>
2024-05-02 12:32:38 -0300

    feat: ensure products of only restaurant can be added to the cart

---
+++ app/_context/cart.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { Prisma } from "@prisma/client";
+import { ReactNode, createContext, useMemo, useState } from "react";
+import { calculateProductTotalPrice } from "../_helpers/price";
+
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          deliveryFee: true;
+        };
+      };
+    };
+  }> {
+  quantity: number;
+}
+
+interface ICartContext {
+  products: CartProduct[];
+  subTotalPrice: number;
+  totalPrice: number;
+  totalDiscounts: number;
+  // eslint-disable-next-line no-unused-vars
+  addProductToCart: ({
+    product,
+    quantity,
+    emptyCart,
+  }: {
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>;
+    quantity: number;
+    emptyCart?: boolean;
+  }) => void;
+  // eslint-disable-next-line no-unused-vars
+  decreaseProductQuantity: (productId: string) => void;
+  // eslint-disable-next-line no-unused-vars
+  increaseProductQuantity: (productId: string) => void;
+  // eslint-disable-next-line no-unused-vars
+  removeProductFromCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  subTotalPrice: 0,
+  totalPrice: 0,
+  totalDiscounts: 0,
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subTotalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscounts = subTotalPrice - totalPrice;
+
+  const addProductToCart = ({
+    product,
+    quantity,
+    emptyCart,
+  }: {
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>;
+    quantity: number;
+    emptyCart?: boolean;
+  }) => {
+    if (emptyCart) {
+      setProducts([]);
+    }
+
+    const isProductAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (isProductAlreadyOnCart) {
+      return setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+
+      return;
+    }
+
+    setProducts((prev) => [...prev, { ...product, quantity }]);
+  };
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          if (cartProduct.quantity === 1) {
+            return cartProduct;
+          }
+
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    return setProducts((prev) =>
+      prev.filter((cartProduct) => cartProduct.id !== productId),
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductFromCart,
+        subTotalPrice,
+        totalPrice,
+        totalDiscounts,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
