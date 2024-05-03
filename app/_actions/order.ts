109d19e2ec73d6fc601f8c33502fe0745686c12d
Matheus Amon <amonmatheus757@gmail.com>
2024-05-03 12:26:20 -0300

    feat: add orders page

--- app/_actions/order.ts
+++ app/_actions/order.ts
@@ -2,8 +2,10 @@
 
 import { Prisma } from "@prisma/client";
 import { db } from "../_lib/prisma";
+import { revalidatePath } from "next/cache";
 
 //tipando a validção da entrada de dados com o prisma
 export const createOrder = async (data: Prisma.OrderCreateInput) => {
-  return db.order.create({ data });
+  await db.order.create({ data });
+  revalidatePath("/my-orders");
 };
---
+++ app/my-orders/_components/order-item.tsx
@@ -0,0 +1,111 @@
+import { Avatar, AvatarImage } from "@/app/_components/ui/avatar";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Separator } from "@/app/_components/ui/separator";
+import { formatCurrency } from "@/app/_helpers/price";
+import { OrderStatus, Prisma } from "@prisma/client";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+
+interface OrderItemProps {
+  order: Prisma.OrderGetPayload<{
+    include: {
+      restaurant: true;
+      products: {
+        include: {
+          product: true;
+        };
+      };
+    };
+  }>;
+}
+
+const getOrderStatusLabel = (status: OrderStatus) => {
+  switch (status) {
+    case "CANCELED":
+      return "Cancelado";
+    case "COMPLETED":
+      return "Entregue";
+    case "CONFIRMED":
+      return "Confirmado";
+    case "DELIVERING":
+      return "Saiu para entrega";
+    case "PREPARING":
+      return "Em preparo";
+  }
+};
+
+const OrderItem = ({ order }: OrderItemProps) => {
+  return (
+    <Card>
+      <CardContent className="p-5">
+        <div
+          className={`w-fit rounded-full bg-[#EEEEEE] px-2 py-1 text-muted-foreground ${order.status !== "COMPLETED" && "bg-green-500 text-white"}`}
+        >
+          <span className="block text-sm font-semibold">
+            {getOrderStatusLabel(order.status)}
+          </span>
+        </div>
+
+        <div className="item-center flex justify-between pt-3">
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src={order.restaurant.imageUrl} />
+            </Avatar>
+
+            <span className="text-sm font-semibold">
+              {order.restaurant.name}
+            </span>
+          </div>
+
+          <Button variant="link" size="icon" className="h-5 w-5 text-black">
+            <Link href={`/restaurant/${order.restaurantId}`}>
+              <ChevronRightIcon />
+            </Link>
+          </Button>
+        </div>
+
+        <div className="py-3">
+          <Separator />
+        </div>
+
+        <div className="space-y-2">
+          {order.products.map((orderProduct) => (
+            <div key={orderProduct.id} className="flex items-center gap-2">
+              <div className="flex h-5 w-5 items-center justify-center rounded-full bg-muted-foreground">
+                <span className="tex-xs block text-white">
+                  {orderProduct.quantity}
+                </span>
+              </div>
+
+              <span className="block text-xs text-muted-foreground">
+                {orderProduct.product.name}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div className="py-3">
+          <Separator />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <p className="text-sm font-semibold">
+            {formatCurrency(Number(order.totalPrice))}
+          </p>
+
+          <Button
+            variant="ghost"
+            size="sm"
+            disabled={order.status !== "COMPLETED"}
+            className="text-xs text-primary"
+          >
+            Refazer Pedido
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default OrderItem;
