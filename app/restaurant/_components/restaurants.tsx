3878b62721c74d13f5b0e7f8a40b1c18fcdd0f26
Matheus Amon <amonmatheus757@gmail.com>
2024-05-04 12:17:36 -0300

    fix: ensure restaurant page show correctly

--- app/restaurant/_components/restaurants.tsx
+++ app/restaurant/_components/restaurants.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Restaurant } from "@prisma/client";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { notFound, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { searchForRestaurants } from "../_actions/search";
 import Header from "@/app/_components/header";
 import RestaurantItem from "@/app/_components/restaurant-item";
 
-const Restaurants = () => {
+interface RestaurantsProps {
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
+}
+
+const Restaurants = ({ userFavoriteRestaurants }: RestaurantsProps) => {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
   const searchParams = useSearchParams();
@@ -37,6 +41,7 @@ const Restaurants = () => {
               key={restaurant.id}
               restaurant={restaurant}
               className="min-w-full max-w-full"
+              userFavoriteRestaurants={userFavoriteRestaurants}
             />
           ))}
         </div>
--- app/restaurant/page.tsx
+++ app/restaurant/page.tsx
@@ -1,10 +1,23 @@
 import { Suspense } from "react";
 import Restaurants from "./_components/restaurants";
+import { authOptions } from "../_lib/auth";
+import { getServerSession } from "next-auth";
+import { db } from "../_lib/prisma";
 
-const RestaurantsPage = () => {
+const RestaurantsPage = async () => {
+  const session = await getServerSession(authOptions);
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
   return (
     <Suspense>
-      <Restaurants />
+      <Restaurants userFavoriteRestaurants={userFavoriteRestaurants} />
     </Suspense>
   );
 };
