844ba087f23c20e252b08b09ed1ca4c224e0869a
Matheus Amon <amonmatheus757@gmail.com>
2024-05-16 08:45:13 -0300

    style: add padding x and grid layout to favorite restaurants

--- app/my-favorite-restaurants/page.tsx
+++ app/my-favorite-restaurants/page.tsx
@@ -23,11 +23,11 @@ const MyFavoriteRestaurants = async () => {
   return (
     <>
       <Header />
-      <div className="px-6 py-6">
+      <div className="px-6 py-6 lg:px-36">
         <h2 className="mb-6 text-lg font-semibold">
           Restaurantes Recomendados
         </h2>
-        <div className="flex w-full flex-col gap-6">
+        <div className="flex w-full flex-col gap-6 lg:grid lg:grid-cols-3 lg:gap-[21px]">
           {userFavoriteRestaurants.length > 0 ? (
             userFavoriteRestaurants.map(({ restaurant }) => (
               <RestaurantItem
